337744045aee32be92fe8151ceb26d95ee79e3a5
SAJanGAIKwad <sajan.gaikwad21@pccoepune.org>
2023-12-29 16:31:37 +0530

    Backend improved

--- Models/Employee.js
+++ Models/Employee.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const EmployeeSchema = new mongoose.Schema({
     name:String,
-    email:String,
+    email:{type:String, unique:true},
     mobile:Number,
     password:String
 })
