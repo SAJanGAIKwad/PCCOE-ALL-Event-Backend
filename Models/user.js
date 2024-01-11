7698a3fe8b2ed9ded419f68443e37d2994c06b02
SAJanGAIKwad <sajan.gaikwad21@pccoepune.org>
2024-01-11 21:43:39 +0530

    Done

--- Models/user.js
+++ Models/user.js
@@ -6,6 +6,6 @@ const UserSchema = new mongoose.Schema({
     mobile:Number,
     password:String
 })
-const User= mongoose.model('User',UserSchema)
+const UserModel = mongoose.model('User',UserSchema)
 
-export default User;
+export default UserModel;
